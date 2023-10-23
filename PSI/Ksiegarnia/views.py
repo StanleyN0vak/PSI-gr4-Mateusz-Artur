01c652252842f98513647fcf13fef1e2e9aab783
Sulgard <73705012+Sulgard@users.noreply.github.com>
2023-10-23 12:51:58 +0200

    beka

--- PSI/Ksiegarnia/views.py
+++ PSI/Ksiegarnia/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core.exceptions import PermissionDenied
+
+
+def index(request):
+    """homepage of learning logs"""
+    return render(request,'Ksiegarnia/index.html')
