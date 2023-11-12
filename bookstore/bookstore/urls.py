ed29815beb90a90999c5566d36f609f76ec93d4d
sulgard <artur.sutula90@gmail.com>
2023-11-12 16:18:43 +0100

    Added another app as I should've done...

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
     path('about', views.about),
     path('', views.homepage),
     path('bookstore', views.bookstore)
