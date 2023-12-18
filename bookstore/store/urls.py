cc7a38fec46ad90dfe1fdd8267154524d1c077c0
Sulgard <73705012+Sulgard@users.noreply.github.com>
2023-12-18 12:32:03 +0100

    Added super user

--- bookstore/store/urls.py
+++ bookstore/store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 from .import views
-
+from .views import HelloWorldView
 
 router = DefaultRouter()
 router.register(r'Address', views.AddressViewSet, basename='Address')
@@ -10,5 +10,6 @@ router.register(r'Address', views.AddressViewSet, basename='Address')
 urlpatterns = [
     path('', views.contents),
     re_path(r'api-auth/', include('rest_framework.urls')),
-    re_path(r'api/', include(router.urls))
+    re_path(r'api/', include(router.urls)),
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
 ]
--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 def contents(request):
-    return render(request,'store/contents.html')
+    return render(request, 'store/contents.html')
 
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
 
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
