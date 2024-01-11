54f8ae895c37dde478637694e25cfb3c75462efa
Sulgard <artur.sutula90@gmail.com>
2024-01-11 21:16:40 +0100

    added filters and ordering

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, generics
 from .models import *
 from .serializers import *
 from rest_framework.permissions import IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class UserListCreateView(generics.ListCreateAPIView):
@@ -19,34 +21,58 @@ class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = [IsAdminUser]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['author_last_name']
+
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author_last_name']
+
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['genre_name']
+
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['publisher_name']
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['title', 'author']
+    ordering_fields = ['publication_date', 'price']
+
 
 class OrderDetailsViewSet(viewsets.ModelViewSet):
     queryset = OrderDetails.objects.all()
     serializer_class = OrderDetailsSerializer
     permission_classes = [IsAdminUser]
+    filter_backends = [OrderingFilter]
+    filterset_fields = ['order_date']
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAdminUser]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['total_price']
+
 
 class OpinionViewSet(viewsets.ModelViewSet):
     queryset = Opinion.objects.all()
     serializer_class = OpinionSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['rating']
